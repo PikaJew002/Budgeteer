015f22b93ae4e097e028d31842c2375805414b1b
Aaron <aaron@ironmthome.com>
2020-05-04 10:07:55 -0400

    Feature, backend: notify when paid

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\NotificationController;
 
 class Kernel extends ConsoleKernel
 {
@@ -25,8 +26,12 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        /* check for notifications to send */
+        $notifications = new NotificationController;
+        /* fetch jobs */
         $jobs = DB::table('jobs')->get();
         $schedule->command('queue:work --stop-when-empty')->when(function() use ($jobs) {
+            /* process all jobs in the queue when there are jobs to process */
             return $jobs->isNotEmpty();
         })->withoutOverlapping();
     }
