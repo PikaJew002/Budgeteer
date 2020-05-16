a8ded326f5f68197f681af5f970d60ddd073de37
Aaron <aaron@ironmthome.com>
2020-05-15 23:09:09 -0400

    Front-end: adding contributions to goals UI (MakeGoal.vue)

---
+++ resources/js/components/contributions/MakeContribution.vue
@@ -0,0 +1,198 @@
+<template>
+  <div>
+    <b-modal v-model="showModal" ref="make-contribution-modal" id="make-contribution-modal" title="Make Goal Contribution" centered no-close-on-backdrop>
+      <b-alert :show="message.countDown"
+               dismissible
+               :variant="message.type"
+               fade
+               @dismissed="message.countDown=0"
+               @dismiss-count-down="countDownChanged">
+        {{message.message}}
+      </b-alert>
+      <form @submit.prevent="onSave(contribution)">
+        <h3>{{ monthSpanToString }}</h3>
+        <div class="row">
+          <div class="col form-group">
+            <label for="amount">Amount</label>
+            <div class="input-group">
+              <div class="input-group-prepend">
+                <div class="input-group-text">$</div>
+              </div>
+              <input class="form-control" id="amount" type="text" placeholder="Amount"
+                     v-model="contribution.amount" @blur="formatAmount()"
+                     :class="{ 'is-invalid': $v.contribution.amount.$invalid && !$v.contribution.amount.$pending,
+                               'is-valid': !$v.contribution.amount.$invalid && !$v.contribution.amount.$pending }">
+            </div>
+            <div v-if="!$v.contribution.amount.required" class="invalid-feedback d-block">
+              Amount is required
+            </div>
+            <div v-if="!$v.contribution.amount.validDecimal" class="invalid-feedback d-block">
+              Amount must be a valid decimal ($xx xxx xxx.xx)
+            </div>
+            <div v-if="$v.contribution.amount.validDecimal && !$v.contribution.amount.notZero" class="invalid-feedback d-block">
+              Amount must be greater than zero (0)
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col form-group">
+            <label for="start_on">Start On Date: </label>
+            <input class="form-control"
+                   :class="{ 'is-invalid': $v.contribution.start_on.$invalid && !$v.contribution.start_on.$pending,
+                             'is-valid': !$v.contribution.start_on.$invalid && !$v.contribution.start_on.$pending }"
+                   id="start_on"
+                   type="date"
+                   placeholder="mm/dd/yyyy"
+                   v-model="contribution.start_on">
+            <div v-if="!$v.contribution.start_on.required" class="invalid-feedback">
+              Start On Date is required
+            </div>
+          </div>
+          <div class="col form-group">
+            <label for="end_on">End On Date: </label>
+            <input class="form-control"
+                   :class="{ 'is-invalid': $v.contribution.end_on.$invalid && !$v.contribution.end_on.$pending,
+                             'is-valid': !$v.contribution.end_on.$invalid && !$v.contribution.end_on.$pending }"
+                   id="end_on"
+                   type="date"
+                   placeholder="mm/dd/yyyy"
+                   v-model="contribution.end_on">
+            <div v-if="!$v.contribution.end_on.required" class="invalid-feedback">
+              End On Date is required
+            </div>
+            <div v-if="!$v.contribution.end_on.minDate" class="invalid-feedback">
+              End On Date must be after the Start On Date
+            </div>
+          </div>
+        </div>
+      </form>
+      <template slot="modal-footer">
+        <b-button size="sm" variant="sub1" @click="$emit('close')">
+          Cancel
+        </b-button>
+        <b-button size="sm" variant="base" @click="onSave(contribution)">
+          Save
+        </b-button>
+      </template>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+  import { BModal, BAlert, BButton } from 'bootstrap-vue';
+  import { helpers, required, minLength, maxLength } from 'vuelidate/lib/validators';
+  import { cloneDeep } from 'lodash';
+  import moment from 'moment';
+  import Alert from '../../api/alert.js';
+  import { EventBus } from '../../event-bus.js';
+  const validDecimal = helpers.regex('validDecimal', /^\d{0,6}(\.\d{0,2})?$/); // double(8,2)
+  export default {
+    components: {
+      'b-modal': BModal,
+      'b-alert': BAlert,
+      'b-button': BButton,
+    },
+    props: {
+      user: {
+        type: Object,
+      },
+      show: {
+        type: Boolean,
+        required: true,
+      },
+    },
+    mixins: [Alert],
+    data() {
+      return {
+        contribution: {
+          amount: null,
+          start_on: "",
+          end_on: "",
+        },
+      };
+    },
+    validations() {
+      return {
+        contribution: {
+          amount: {
+            required,
+            validDecimal,
+            notZero: (amount) => ((amount == "" || amount == null) || (Number(amount) > 0)), // allows null and ""
+          },
+          start_on: {
+            required,
+          },
+          end_on: {
+            required,
+            minDate: (end_on) => (end_on == "" || moment(end_on).isAfter(this.contribution.start_on))
+          },
+        },
+      };
+    },
+    created() {
+      EventBus.$on('make-contribution', () => {
+        this.contribution.amount = null;
+        this.contribution.start_on = "";
+        this.contribution.end_on = "";
+        this.showModal = true;
+      });
+      EventBus.$on('show-contribution-alert', (messageArr) => {
+        this.showAlert(messageArr[0], messageArr[1], messageArr[2]);
+      });
+    },
+    beforeDestroy() {
+      EventBus.$off('make-contribution');
+      EventBus.$off('show-contribution-alert');
+    },
+    methods: {
+      onSave(contribution) {
+        if(!this.$v.contribution.$invalid) {
+          let newContribution = cloneDeep(contribution);
+          newContribution.monthSpan = this.monthSpan;
+          newContribution.diff = Math.ceil(moment(newContribution.end_on).diff(newContribution.start_on, 'months', true));
+          this.$emit('save', newContribution);
+        }
+      },
+      formatAmount() {
+        if(Number(this.contribution.amount).toFixed(2) != "NaN" && this.contribution.amount != "" && this.contribution.amount != null) {
+          this.contribution.amount = Number(this.contribution.amount).toFixed(2);
+        }
+      },
+    },
+    computed: {
+      showModal: {
+        get() {
+          return this.show;
+        },
+        set(value) {
+          if(value) {
+            this.$emit('open');
+          } else {
+            this.$emit('close');
+          }
+        },
+      },
+      monthSpan() {
+        if(!this.$v.contribution.$invalid) {
+          if(moment(this.contribution.start_on).isSame(this.contribution.end_on, 'month')) {
+            return [moment(this.contribution.start_on).format('MMM YYYY')];
+          } else {
+            return [moment(this.contribution.start_on).format('MMM YYYY'), moment(this.contribution.end_on).format('MMM YYYY')]
+          }
+        } else {
+          return [];
+        }
+      },
+      monthSpanToString() {
+        if(this.monthSpan.length > 0) {
+          if(this.monthSpan.length > 1) {
+            return this.monthSpan[0] + " - " + this.monthSpan[1];
+          }
+          return this.monthSpan[0];
+        }
+        return "";
+      },
+
+    },
+  };
+</script>
--- resources/js/components/goals/MakeGoal.vue
+++ resources/js/components/goals/MakeGoal.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <make-contribution :show="showMakeContribution"
+                       @open="showMakeContribution = true"
+                       @close="showMakeContribution = false"
+                       @save="onContributionSave"></make-contribution>
     <b-alert :show="message.countDown"
              dismissible
              :variant="message.type"
@@ -8,39 +12,95 @@
              @dismiss-count-down="countDownChanged">
       {{message.message}}
     </b-alert>
-    <b-modal v-model="showModal" ref="make-goal-modal" id="make-goal-modal" title="Make Goal" centered no-close-on-backdrop>
-      <form @submit.prevent="onSave(goal)">
-        <div class="form-group">
-          <label for="name">Name</label>
-          <input class="form-control" :class="{ 'is-invalid': $v.goal.name.$invalid && !$v.goal.name.$pending,
-                                                'is-valid': !$v.goal.name.$invalid && !$v.goal.name.$pending }"
-                 id="name" type="text" placeholder="Goal Name" v-model="goal.name">
-          <div v-if="!$v.goal.name.required" class="invalid-feedback">
-            Name is required
-          </div>
-          <div v-if="!$v.goal.name.minLength" class="invalid-feedback">
-            Name must be at least 2 characters
-          </div>
-          <div v-if="!$v.goal.name.maxLength" class="invalid-feedback">
-            Name cannot be more than 50 characters
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="amount">Amount</label>
-          <div class="input-group">
-            <div class="input-group-prepend">
-              <div class="input-group-text">$</div>
+    <b-modal v-model="showModal" ref="make-goal-modal" id="make-goal-modal" title="Make Goal" size="lg" centered no-close-on-backdrop>
+      <div class="container-fluid">
+        <form @submit.prevent="onSave(goal)">
+          <div class="row">
+            <div class="col form-group">
+              <label for="name">Name</label>
+              <input class="form-control" :class="{ 'is-invalid': $v.goal.name.$invalid && !$v.goal.name.$pending,
+                                                    'is-valid': !$v.goal.name.$invalid && !$v.goal.name.$pending }"
+                     id="name" type="text" placeholder="Goal Name" v-model="goal.name">
+              <div v-if="!$v.goal.name.required" class="invalid-feedback">
+                Name is required
+              </div>
+              <div v-if="!$v.goal.name.minLength" class="invalid-feedback">
+                Name must be at least 2 characters
+              </div>
+              <div v-if="!$v.goal.name.maxLength" class="invalid-feedback">
+                Name cannot be more than 50 characters
+              </div>
             </div>
-            <input class="form-control" id="amount" type="text" placeholder="Amount"
-                   v-model="goal.amount" @blur="formatAmount()"
-                    :class="{ 'is-invalid': $v.goal.amount.$invalid && !$v.goal.amount.$pending,
-                              'is-valid': !$v.goal.amount.$invalid && !$v.goal.amount.$pending }">
           </div>
-          <div v-if="!$v.goal.amount.validDecimal" class="invalid-feedback d-block">
-            Amount must be a valid decimal ($xx xxx xxx.xx)
+          <div class="row">
+            <div class="col form-group">
+              <label for="amount">Amount</label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <div class="input-group-text">$</div>
+                </div>
+                <input class="form-control" id="amount" type="text" placeholder="Amount"
+                       v-model="goal.amount" @blur="formatAmount()"
+                       :class="{ 'is-invalid': $v.goal.amount.$invalid && !$v.goal.amount.$pending,
+                                 'is-valid': !$v.goal.amount.$invalid && !$v.goal.amount.$pending }">
+              </div>
+              <div v-if="!$v.goal.amount.required" class="invalid-feedback d-block">
+                Amount is required
+              </div>
+              <div v-if="!$v.goal.amount.validDecimal" class="invalid-feedback d-block">
+                Amount must be a valid decimal ($xx xxx xxx.xx)
+              </div>
+              <div v-if="$v.goal.amount.validDecimal && !$v.goal.amount.notZero" class="invalid-feedback d-block">
+                Amount must be greater than zero (0.00)
+              </div>
+            </div>
           </div>
-        </div>
-      </form>
+          <div class="row">
+            <div class="col form-group">
+              <label for="initial_amount">Initial Amount (optional)</label>
+              <div class="input-group">
+                <div class="input-group-prepend">
+                  <div class="input-group-text">$</div>
+                </div>
+                <input class="form-control" id="initial_amount" type="text" placeholder="Amount"
+                       v-model="goal.initial_amount" @blur="formatInitialAmount()"
+                       :class="{ 'is-invalid': $v.goal.initial_amount.$invalid && !$v.goal.initial_amount.$pending,
+                                 'is-valid': !$v.goal.initial_amount.$invalid && !$v.goal.initial_amount.$pending }">
+              </div>
+              <div v-if="!$v.goal.initial_amount.validDecimal" class="invalid-feedback d-block">
+                Initial Amount must be a valid decimal ($xx xxx xxx.xx)
+              </div>
+              <div v-if="$v.goal.initial_amount.validDecimal && !$v.goal.initial_amount.notZero" class="invalid-feedback d-block">
+                Initial Amount must be greater than zero (0.00)
+              </div>
+            </div>
+          </div>
+          <div class="d-flex justify-content-between align-items-center">
+            <h6>Contributions</h6><button type="button" class="btn btn-outline-base" @click="onAddContribution()">+</button>
+          </div>
+          <hr v-if="goal.contributions.length > 0">
+          <div class="d-flex flex-row flex-nowrap overflow-auto">
+            <div v-for="(contribution, index) in goal.contributions"
+                 :key="contribution.start_on"
+                 class="card p-3 mx-2 my-0 border border-base rounded-sm"
+                 style="min-width: 100px;">
+              <h6 class="card-title">{{ contribution.monthSpan[0] + (contribution.monthSpan.length > 1 ? " - " + contribution.monthSpan[1] : "") }}</h6>
+              <h6 v-if="contribution.diff > 1" class="card-subtitle mb-2 text-muted">${{ contribution.amount }} x {{ contribution.diff }} months = ${{ (contribution.diff*Number(contribution.amount)).toFixed(2) }}</h6>
+              <h6 v-else class="card-subtitle mb-2 text-muted">${{ contribution.amount }} x 1 month</h6>
+              <button class="btn btn-outline-sub1 btn-sm" @click="onDeleteContribution(index)">Delete</button>
+            </div>
+          </div>
+          <hr v-if="goal.contributions.length > 0">
+          <template v-if="goal.amount != null && goal.amount != ''">
+            <span v-if="goal.contributions.length > 0 && (goal.initial_amount == null || goal.initial_amount == '') ">
+              ${{ contributionsTotal }} / ${{ goal.amount }} = {{ ((Number(contributionsTotal) / Number(goal.amount))*100).toFixed(2) }}%
+            </span>
+            <span v-if="goal.contributions.length > 0 && (goal.initial_amount != null && goal.initial_amount != '') ">
+              ${{ goal.initial_amount }} + ${{ contributionsTotal }} / ${{ goal.amount }} = {{ (((Number(contributionsTotal) + Number(goal.initial_amount)) / Number(goal.amount))*100).toFixed(2) }}%
+            </span>
+          </template>
+        </form>
+      </div>
       <template slot="modal-footer">
         <b-button size="sm" variant="sub1" @click="$emit('close')">
           Cancel
@@ -54,16 +114,21 @@
 </template>
 
 <script>
-  import { BModal, BAlert, BButton } from 'bootstrap-vue';
+  import { BModal, BAlert, BButton, BContainer } from 'bootstrap-vue';
   import { helpers, required, minLength, maxLength } from 'vuelidate/lib/validators';
+  import moment from 'moment';
+  import { cloneDeep } from 'lodash';
+  import MakeContribution from '../contributions/MakeContribution.vue';
   import Alert from '../../api/alert.js';
   import { EventBus } from '../../event-bus.js';
-  const validDecimal = helpers.regex('validDecimal', /^\d{0,8}(\.\d{0,2})?$/);
+  const validDecimal = helpers.regex('validDecimal', /^\d{0,8}(\.\d{0,2})?$/); // double(10,2)
   export default {
     components: {
       'b-modal': BModal,
       'b-alert': BAlert,
       'b-button': BButton,
+      'b-container': BContainer,
+      'make-contribution': MakeContribution,
     },
     props: {
       user: {
@@ -77,28 +142,43 @@
     mixins: [Alert],
     data() {
       return {
+        showMakeContribution: false,
         goal: {
           name: "",
           amount: null,
+          initial_amount: null,
+          contributions: [],
         },
+        contributionsSpan: [],
       };
     },
-    validations: {
-      goal: {
-        name: {
-          required,
-          minLength: minLength(2),
-          maxLength: maxLength(255),
+    validations() {
+      return {
+        goal: {
+          name: {
+            required,
+            minLength: minLength(2),
+            maxLength: maxLength(255),
+          },
+          amount: {
+            required,
+            validDecimal,
+            notZero: (amount) => ((amount == "" || amount == null) || (Number(amount) > 0)), // allows null and ""
+          },
+          initial_amount: {
+            validDecimal,
+            notZero: (amount) => ((amount == "" || amount == null) || (Number(amount) > 0)), // allows null and ""
+          },
         },
-        amount: {
-          validDecimal,
-        },
-      },
+      };
     },
     created() {
       EventBus.$on('make-goal', () => {
         this.goal.name = "";
         this.goal.amount = null;
+        this.goal.initial_amount = null;
+        this.goal.contributions = [];
+        this.contributionsSpan = [];
         this.showModal = true;
       });
     },
@@ -106,6 +186,30 @@
       EventBus.$off('make-goal');
     },
     methods: {
+      onAddContribution() {
+        EventBus.$emit('make-contribution');
+      },
+      onDeleteContribution(index) {
+        if(this.goal.contributions.length == 1) {
+          this.contributionsSpan = [];
+        } else {
+          if(this.contributionsSpan[0] == this.goal.contributions[index].start_on) {
+            this.contributionsSpan[0] = this.goal.contributions[index + 1].start_on;
+          } else if(this.contributionsSpan[1] == this.goal.contributions[index].end_on) {
+            this.contributionsSpan[1] = this.goal.contributions[index - 1].end_on;
+          }
+        }
+        this.goal.contributions.splice(index, 1);
+      },
+      onContributionSave(contribution, event) {
+        let checkStatus = this.checkOverLapAndAdd(contribution)
+        if(checkStatus[0]) {
+          this.goal.contributions.push(contribution);
+          this.showMakeContribution = false;
+        } else {
+          EventBus.$emit('show-contribution-alert', ['danger', checkStatus[1], 15]);
+        }
+      },
       onSave(goal) {
         if(!this.$v.goal.$invalid) {
           this.$store.dispatch('addGoal', goal);
@@ -113,9 +217,40 @@
         }
       },
       formatAmount() {
-        if(Number(this.goal.amount).toFixed(2) != "NaN") {
+        if(Number(this.goal.amount).toFixed(2) != "NaN" && this.goal.amount != "" && this.goal.amount != null) {
           this.goal.amount = Number(this.goal.amount).toFixed(2);
         }
+      },
+      formatInitialAmount() {
+        if(Number(this.goal.initial_amount).toFixed(2) != "NaN" && this.goal.initial_amount != "" && this.goal.initial_amount != null) {
+          this.goal.initial_amount = Number(this.goal.initial_amount).toFixed(2);
+        }
+      },
+      checkOverLapAndAdd(contribution) {
+        if(this.contributionsSpan.length == 0) {
+          this.contributionsSpan[0] = contribution.start_on;
+          this.contributionsSpan[1] = contribution.end_on;
+          return [true, ''];
+        }
+        if(this.contributionsSpan.length == 2) {
+          if(moment(contribution.start_on).isBetween(this.contributionsSpan[0], this.contributionsSpan[1], 'month', "[]")) {
+            return [false, '\'Start On\' overlaps with other scheduled contributions!'];
+          }
+          if(moment(contribution.end_on).isBetween(this.contributionsSpan[0], this.contributionsSpan[1], 'month', "[]")) {
+            return [false, '\'End On\' overlaps with other scheduled contributions!'];
+          }
+          if(moment(contribution.start_on).isBefore(this.contributionsSpan[0], 'month') && moment(contribution.end_on).isBefore(this.contributionsSpan[0], 'month')) {
+            // is before contributionsSpan, update lower boundry with contribution.start_on
+            this.contributionsSpan[0] = contribution.start_on;
+            return [true, ''];
+          }
+          if(moment(contribution.end_on).isAfter(this.contributionsSpan[1], 'month') && moment(contribution.start_on).isAfter(this.contributionsSpan[1], 'month')) {
+            // is after contributionsSpan, update upper boundry with contribution.end_on
+            this.contributionsSpan[1] = contribution.end_on;
+            return [true, ''];
+          }
+        }
+        return [false, 'Something went wrong... reload the page and try again.'];
       },
     },
     computed: {
@@ -130,6 +265,22 @@
             this.$emit('close');
           }
         },
+      },
+      goalContributions() {
+        return this.goal.contributions.sort(function(a, b) {
+          if(moment(a.start_on).isBefore(b.start_on, 'month')) {
+            return -1;
+          } else {
+            return 1;
+          }
+        });
+      },
+      contributionsTotal() {
+        let total = 0;
+        for(let i in this.goal.contributions) {
+          total += (Number(this.goal.contributions[i].amount)*this.goal.contributions[i].diff);
+        }
+        return total.toFixed(2);
       },
     },
   };
