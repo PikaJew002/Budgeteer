57ab7f372e4e2db6e09fbe5354916c6b47167a8d
Aaron <aaron@ironmthome.com>
2020-05-18 16:02:04 -0400

    Back-end(policy): contribution-paycheck association to paycheck

--- app/Policies/PaycheckPolicy.php
+++ app/Policies/PaycheckPolicy.php
@@ -141,4 +141,48 @@ class PaycheckPolicy
     {
         return $paycheck->income->user_id == $user->id && $bill->user_id == $user->id;
     }
+
+    /**
+     * The following methods are for Contribution-Paycheck association management
+     */
+
+    /**
+     * Determine whether the user can create the contribution-paycheck association
+     *
+     * @param  \App\User  $user
+     * @param  \App\Paycheck  $paycheck
+     * @param  \App\Contribution  $contribution
+     * @return mixed
+     */
+    public function attachContribution(User $user, Paycheck $paycheck, Contribution $contribution)
+    {
+        return $user->id == $paycheck->income->user_id && $user->id == $contribution->goal->user_id;
+    }
+
+    /**
+     * Determine whether the user can update the contribution-paycheck association
+     *
+     * @param  \App\User  $user
+     * @param  \App\Paycheck  $paycheck
+     * @param  \App\Contribution  $contribution
+     * @return mixed
+     */
+
+    public function updatePivotContribution(User $user, Paycheck $paycheck, Contribution $contribution)
+    {
+        return $user->id == $paycheck->income->user_id && $user->id == $contribution->goal->user_id;
+    }
+
+    /**
+     * Determine whether the user can delete the contribution-paycheck association
+     *
+     * @param  \App\User  $user
+     * @param  \App\Paycheck  $paycheck
+     * @param  \App\Contribution  $contribution
+     * @return mixed
+     */
+    public function detachContribution(User $user, Paycheck $paycheck, Contribution $contribution)
+    {
+        return $user->id == $paycheck->income->user_id && $user->id == $contribution->goal->user_id;
+    }
 }
