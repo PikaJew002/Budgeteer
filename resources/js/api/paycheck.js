ae0dbb47d4598c33404663eae4d27cee391caf7d
Aaron <aaron@ironmthome.com>
2020-05-21 00:31:33 -0400

    Frontend(api, bill|income|paycheck): standardized args/doc comments

--- resources/js/api/paycheck.js
+++ resources/js/api/paycheck.js
@@ -24,26 +24,36 @@ export default {
   /*
     GET   /api/paycheck/{id}
     @param id int
+    @param options {object}
+      can contain with [array]
     @return Promise
   */
-  getPaycheck: function(id) {
-    return axios.get(BUDGETEER_CONFIG.API_URL + '/paycheck/' + id);
+  getPaycheck: function(id, options = {}) {
+    let optionsStr = '?';
+    if(options.hasOwnProperty('with') && options.with.length != 0) {
+      optionsStr += (optionsStr == '?' ? 'with=' + options.with[0] : '&with=' + options.with[0]);
+      for(let i in options.with) {
+        if(i == 0) continue;
+        optionsStr += ':' + options.with[i];
+      }
+    }
+    return axios.get(BUDGETEER_CONFIG.API_URL + '/paycheck/' + id + (optionsStr == '?' ? '' : optionsStr));
   },
   /*
     POST   /api/paycheck
-    @param data object
+    @param paycheck object
     @return Promise
   */
-  postPaycheck: function(data) {
-    return axios.post(BUDGETEER_CONFIG.API_URL + '/paycheck', data);
+  postPaycheck: function(paycheck) {
+    return axios.post(BUDGETEER_CONFIG.API_URL + '/paycheck', paycheck);
   },
   /*
     PUT     /api/paycheck
     @param data object
     @return Promise
   */
-  putPaycheck: function(data) {
-    return axios.put(BUDGETEER_CONFIG.API_URL + '/paycheck', data);
+  putPaycheck: function(paycheck) {
+    return axios.put(BUDGETEER_CONFIG.API_URL + '/paycheck', paycheck);
   },
   /*
     DELETE   /api/paycheck/{id}
@@ -53,29 +63,4 @@ export default {
   deletePaycheck: function(id) {
     return axios.delete(BUDGETEER_CONFIG.API_URL + '/paycheck/' + id);
   },
-  /*
-    POST    /api/billpaycheck
-    @param data object
-    @return Promise
-  */
-  postBillPaycheck: function(data) {
-    return axios.post(BUDGETEER_CONFIG.API_URL + '/billpaycheck', data);
-  },
-  /*
-    PUT     /api/billpaycheck
-    @param data object
-    @return Promise
-  */
-  putBillPaycheck: function(data) {
-    return axios.put(BUDGETEER_CONFIG.API_URL + '/billpaycheck', data);
-  },
-  /*
-    DELETE  /api/billpaycheck/{billId}/{paycheckId}
-    @param bill_id int
-    @param paycheck_id int
-    @return Promise
-  */
-  deleteBillPaycheck: function(bill_id, paycheck_id) {
-    return axios.delete(BUDGETEER_CONFIG.API_URL + '/billpaycheck/' + bill_id + '/' + paycheck_id);
-  }
 }
