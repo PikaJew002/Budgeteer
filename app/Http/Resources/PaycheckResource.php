f772a43a92f443521ecb26d47a4256895ed46193
Aaron <aaron@ironmthome.com>
2020-06-07 19:23:46 -0400

    be(resource, Paycheck): changed pivot_* to contribution_* or bill_*

--- app/Http/Resources/PaycheckResource.php
+++ app/Http/Resources/PaycheckResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Resources\Json\JsonResource;
 use App\Http\Resources\IncomeResource;
 use App\Http\Resources\BillResource;
+use App\Http\Resources\ContributionResource;
 
 class PaycheckResource extends JsonResource
 {
@@ -21,18 +22,19 @@ class PaycheckResource extends JsonResource
             'income_id' => $this->income_id,
             'income' => new IncomeResource($this->whenLoaded('income')),
             'bills' => BillResource::collection($this->whenLoaded('bills')),
-            'pivot_amount' => $this->whenPivotLoaded('bill_paycheck', function() {
+            'bill_amount' => $this->whenPivotLoaded('bill_paycheck', function() {
                 return $this->pivot->amount;
             }),
-            'pivot_amount_project' => $this->whenPivotLoaded('bill_paycheck', function() {
+            'bill_amount_project' => $this->whenPivotLoaded('bill_paycheck', function() {
                 return $this->pivot->amount_project;
             }),
-            'pivot_due_on' => $this->whenPivotLoaded('bill_paycheck', function() {
+            'bill_due_on' => $this->whenPivotLoaded('bill_paycheck', function() {
                 return $this->pivot->due_on;
             }),
-            'pivot_paid_on' => $this->whenPivotLoaded('bill_paycheck', function() {
+            'bill_paid_on' => $this->whenPivotLoaded('bill_paycheck', function() {
                 return $this->pivot->paid_on;
             }),
+            'contributions' => ContributionResource::collection($this->whenLoaded('contributions')),
             'contribution_amount' => $this->whenPivotLoaded('contribution_paycheck', function() {
                 return $this->pivot->amount;
             }),
@@ -51,7 +53,7 @@ class PaycheckResource extends JsonResource
             'notify_when_paid' => $this->notify_when_paid,
             'paid_on' => $this->paid_on,
             'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at
+            'updated_at' => $this->updated_at,
         ];
     }
 }
