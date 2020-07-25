2d51dae93ea29a0e6d33059b51009f126f1319f2
Aaron <aaron@ironmthome.com>
2020-07-25 01:25:03 -0400

    BE(notifications,PaycheckPaid): include contributions, "left over" calc

--- app/Notifications/PaycheckPaid.php
+++ app/Notifications/PaycheckPaid.php
@@ -51,22 +51,47 @@ class PaycheckPaid extends Notification implements ShouldQueue
      */
     public function toMail($notifiable)
     {
-        $this->paycheck->load(['income', 'bills']);
+        $this->paycheck->load(['income', 'bills', 'contributions.goal']);
         $message = (new MailMessage)->line('You\'ve been paid from '.$this->paycheck->income->name.'!');
         // if the amount is projected
         if($this->paycheck->amount == null) {
             $message = $message->line('We see you marked this paycheck as projected.')
                                ->line('You should update the paycheck with the correct amount to see how much you actually have left over.');
         }
-        if($this->paycheck->bills->isNotEmpty()) {
-            $message = $message->line('Now you\'ve been paid, you can pay those bills!');
-            foreach($this->paycheck->bills as $bill) {
-                $message = $message->line(' - '.$bill->name.' $'.($bill->pivot->amount == null ? $bill->pivot->amount_project : $bill->pivot->amount));
+        $billsNotEmpty = $this->paycheck->bills->isNotEmpty();
+        $contributionsNotEmpty = $this->paycheck->contributions->isNotEmpty();
+        if($billsNotEmpty || $contributionsNotEmpty) {
+            $billsAmount = 0;
+            if($billsNotEmpty) {
+                $message = $message->line('Bills coming out of this paycheck:');
+                foreach($this->paycheck->bills as $bill) {
+                    $amount = $bill->pivot->amount == null ? $bill->pivot->amount_project : $bill->pivot->amount;
+                    $billsAmount = $billsAmount + $amount;
+                    $message = $message->line(' - '.$bill->name.' $'.$amount);
+                }
             }
-            $message = $message->line('Remember to log in and mark those bills as paid.');
+            $contributionsAmount = 0;
+            if($contributionsNotEmpty) {
+                $message = $message->line('Contributions coming out of this paycheck:');
+                foreach($this->paycheck->contributions as $contribution) {
+                    $amount = $contribution->pivot->amount == null ? $contribution->pivot->amount_project : $contribution->pivot->amount;
+                    $contributionsAmount = $contributionsAmount + $amount;
+                    $message = $message->line(' - '.$contribution->goal->name.' $'.$amount);
+                }
+            }
+            if($this->paycheck->amount == null) {
+                $leftOver = $this->paycheck->amount_project - $billsAmount - $contributionsAmount;
+                $message = $message->line('Projected left over (after bills and contributions):');
+                $message = $message->line('$'.$leftOver);
+            } else {
+                $leftOver = $this->paycheck->amount - $billsAmount - $contributionsAmount;
+                $message = $message->line('Left over (after bills and contributions):');
+                $message = $message->line('$'.$leftOver);
+            }
+            $message = $message->line('Remember to log in and mark those bills/contributions as paid.');
         } else {
-            $message = $message->line('Hmmm, you don\'t seem to have any bills assigned to this paycheck.')
-                               ->line('Log in to assign any bills, pay them, and mark them as paid (or don\'t, it\'s your money, haha).');
+            $message = $message->line('Hmmm, you don\'t seem to have any bills or contributions assigned to this paycheck.')
+                               ->line('Log in to assign any bills or contributions, pay (or save) them, and mark them as paid (or don\'t, it\'s your money, haha).');
         }
         return $message->action('Log in', url('/login'))
                            ->line('Thank you for using our application!');
