df8f36e7312c5e2cc86918fca0dc11debe089868
Aaron <aaron@ironmthome.com>
2020-08-19 16:45:03 -0400

    FE(views,user_verified): updated page view to match background

--- resources/views/user_verified.blade.php
+++ resources/views/user_verified.blade.php
@@ -3,10 +3,16 @@
 @section('content')
     <main class="py-2">
       <div class="container">
-        <h2>User Verified!</h2>
-        <p>
-          The User {{ $user->name }} ({{ $user->email }}) was verified and has been notified.
-        </p>
+        <div class="card bg-transparent">
+          <div class="card-header bg-smokewhite-100">
+            <h2>User Verified!</h2>
+          </div>
+          <div class="card-body bg-white-50">
+            <p>
+              The User {{ $user->name }} ({{ $user->email }}) was verified and has been notified.
+            </p>
+          </div>
+        </div>
       </div>
     </main>
 @endsection
