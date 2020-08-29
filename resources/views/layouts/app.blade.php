6a3ed5947df2cf9e875f39a69e6a108bdaf6d2c6
Aaron <aaron@ironmthome.com>
2020-08-28 23:21:08 -0400

    Versioning added to complied files for cache busting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <script type='text/javascript'>
       window.Laravel = <?php echo json_encode(['csrfToken' => csrf_token()]); ?>
     </script>
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ mix('/js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
