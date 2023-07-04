9dfe1250fadba6c00d838bc85ef867a727b52c09
Aaron <aaron@ironmthome.com>
2023-07-04 15:46:37 -0400

    Update Laravel Mix to help reduce npm related errors

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,5 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').version()
+mix.js('resources/js/app.js', 'public/js').vue({ version: 2 }).version()
     .sass('resources/sass/app.scss', 'public/css').disableNotifications();
