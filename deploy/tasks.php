5b8569d935ac086947ac0782c277462d3377b163
Aaron <aaron@ironmthome.com>
2023-07-04 14:19:54 -0400

    Fix typos

--- deploy/tasks.php
+++ deploy/tasks.php
@@ -4,12 +4,12 @@ namespace Deployer;
 
 desc('volta run npm run build');
 task('volta:run:install', function () {
-    cd('{{release_or_current_path');
+    cd('{{release_or_current_path}}');
     run('volta run npm install');
 });
 
 desc('volta run npm install');
 task('volta:run:prod', function () {
-    cd('{{release_or_current_path');
+    cd('{{release_or_current_path}}');
     run('volta run npm run prod');
 });
