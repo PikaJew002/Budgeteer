703b3d2cbf0e239a373e5bf89fbfd83bea1fb06b
Aaron <aaron@ironmthome.com>
2023-07-10 16:53:44 -0400

    Add step for uploading and downloading build artifacts

--- deploy/tasks.php
+++ deploy/tasks.php
@@ -13,3 +13,9 @@ task('volta:run:prod', function () {
     cd('{{release_or_current_path}}');
     run('volta run npm run production');
 });
+
+desc('download build artifacts from Github Actions workflow runn');
+task('gh:action:download', function () {
+    cd('{{release_or_current_path}}');
+    run('gh run download '. getenv('GITHUB_RUN_ID') .' -n npm-build');
+});
