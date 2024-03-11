bd36c451bd587d74f771b8b68964544222fac040
Aaron <aaron@ironmthome.com>
2024-03-11 19:37:15 -0400

    Fix output of Vite generated files (pre @vite directive added in Laravel 9.x)

--- vite.config.js
+++ vite.config.js
@@ -29,13 +29,4 @@ export default defineConfig({
       },
     }),
   ],
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]',
-      },
-    },
-  },
 });
