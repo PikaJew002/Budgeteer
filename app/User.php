9453fec0cb9fca375f0464841c804ebde2fec0c2
Aaron Eisenberg <aaron@ironmthome.com>
2025-01-19 10:57:02 -0500

    Add factory to User model, add seeder

--- app/User.php
+++ app/User.php
@@ -3,13 +3,14 @@
 namespace App;
 
 use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, Notifiable, CanResetPassword;
+    use HasApiTokens, Notifiable, CanResetPassword, HasFactory;
 
     /**
      * The attributes that are mass assignable.
